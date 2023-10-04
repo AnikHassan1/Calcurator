b431f0b4dae49f2020a979afc0e64cb68eee58d8
Anik Hassan1 <ah491606@gmail.com>
2023-10-04 21:41:27 +0600

    first

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// document.addEventListener("click",anik);
+
+
+// function anik(){
+//   let num=  document.getElementById("box");
+//   let num3=  document.getElementById("box2");
+//   let num2=num.innerText;
+//   let num4=num3.innerText;
+//   console.log(num2);
+//   console.log(num4);
+// }
